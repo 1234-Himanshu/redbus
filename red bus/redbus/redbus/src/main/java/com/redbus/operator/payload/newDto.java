f959dbd306ef9f7e8178222275cfef8efb91a731
Himanshu Kumar Singh <himanshukumarsingh521@gmail.com>
2024-06-14 15:14:41 +0530

    Commit new Dto addition1

---
+++ red bus/redbus/redbus/src/main/java/com/redbus/operator/payload/newDto.java
@@ -0,0 +1,14 @@
+package com.redbus.operator.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class newDto {
+    private String seatId;
+    private String seatNumber;
+    private String seatCompanyName;
+}
